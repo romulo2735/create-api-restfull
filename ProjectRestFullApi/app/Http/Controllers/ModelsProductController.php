a28b1d1964149621bf7df1e920cfb233127300f8
Romulo <romulosousa@fabricainfo.com>
2019-06-07 10:06:54 -0300

    Modulo da API para produtos

---
+++ ProjectRestFullApi/app/Http/Controllers/ModelsProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModelsProduct;
+use Illuminate\Http\Request;
+
+class ModelsProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModelsProduct  $modelsProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelsProduct $modelsProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModelsProduct  $modelsProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelsProduct $modelsProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModelsProduct  $modelsProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelsProduct $modelsProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModelsProduct  $modelsProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelsProduct $modelsProduct)
+    {
+        //
+    }
+}
