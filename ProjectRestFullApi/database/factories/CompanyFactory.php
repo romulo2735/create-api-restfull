7e96519924cdf28bbe614e41f73e698d06ad430a
Romulo <romulosousa@fabricainfo.com>
2019-06-10 17:22:44 -0300

    FActory Company

---
+++ ProjectRestFullApi/database/factories/CompanyFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+use App\Models\Company;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'fantasyName' => $faker->companyEmail,
+        'email' => $faker->email,
+        'cnpj' => 79846521325468,
+    ];
+});
