a60cc7115ab5befa0f3ac4c8007dcafd84d43868
Romulo Sousa <romulosousa27@hotmail.com>
2019-06-25 22:39:02 -0300

    fillable e relacionamento com company

--- ProjectRestFullApi/app/Models/Job.php
+++ ProjectRestFullApi/app/Models/Job.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo as BelongsToAlias;
 
+/**
+ * Class Job
+ * @package App\Models
+ */
 class Job extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'salary', 'hours', 'quantity', 'status', 'company_id'
+    ];
+
+    /**
+     * @return BelongsToAlias
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
