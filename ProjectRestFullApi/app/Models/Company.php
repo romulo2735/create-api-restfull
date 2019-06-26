3caa176895ed741485bf1e8eef4e3b93a46d8b1a
Romulo Sousa <romulosousa27@hotmail.com>
2019-06-25 22:38:47 -0300

    definindo o relacionamento entre empresa e vagas

--- ProjectRestFullApi/app/Models/Company.php
+++ ProjectRestFullApi/app/Models/Company.php
@@ -3,12 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany as HasManyAlias;
 
+/**
+ * Class Company
+ * @package App\Models
+ */
 class Company extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = [
-        'fantasyName',
-        'email',
-        'cnpj',
+        'fantasyName', 'email', 'cnpj',
     ];
+
+    /**
+     * @return HasManyAlias
+     */
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
